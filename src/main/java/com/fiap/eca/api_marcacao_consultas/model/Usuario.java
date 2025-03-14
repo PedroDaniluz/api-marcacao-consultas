f9a8f08538bc7916c295b58d10516961a72a49c4
pedrodaniluz <pdaniluz.dev@gmail.com>
2025-03-14 10:10:06 -0300

    build: jwtToken, pom and properties fix - usuario model settings

--- src/main/java/com/fiap/eca/api_marcacao_consultas/model/Usuario.java
+++ src/main/java/com/fiap/eca/api_marcacao_consultas/model/Usuario.java
@@ -1,6 +1,7 @@
 package com.fiap.eca.api_marcacao_consultas.model;
 import jakarta.persistence.*;
 import lombok.Data;
+import com.fiap.eca.api_marcacao_consultas.dto.LoginRequest;
 @Data
 @Entity
 @Table(name = "usuarios")
